4292b486f1e2455f0ba0b16fb412461b8389321e
Phuc-EroDev <phucvo140902@gmail.com>
2025-05-16 03:56:44 +0700

    make model and connect DB

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const  mongoose = require('mongoose');
 
 dotenv.config();
 
@@ -9,6 +10,15 @@ const port = process.env.PORT || 3001;
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
+
+mongoose.connect(`mongodb+srv://phucvo140902:${process.env.MONGO_DB}@erodb.ri4bf8p.mongodb.net/?retryWrites=true&w=majority&appName=EroDB`)
+.then(() => {
+    console.log('Connected DB Success !!!')
+})
+.catch((err) => {
+    console.log('Connected DB Failed !!!')
+    console.log(err)
+})
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
