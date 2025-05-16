46001630da97144567291ac79de5803f5997de92
Phuc-EroDev <phucvo140902@gmail.com>
2025-05-16 23:28:09 +0700

    make api create user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -2,8 +2,27 @@ const UserService = require('../services/UserService');
 
 const createUser = async(req, res) => {
     try {
-        console.log(req.body);
-        const data = await UserService.createUser();
+        const { name, email, password, confirmPassword, phone } = req.body;
+        const reg = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        const isValidEmail = reg.test(email);
+        if(!name || !email || !password || !confirmPassword || !phone) {
+            return res.status(200).json({ 
+                status: "Error",
+                message: "Please fill all the fields"
+             });
+        } else if (!isValidEmail) {
+            return res.status(200).json({ 
+                status: "Error",
+                message: "Email is not valid"
+             });
+        } else if (password !== confirmPassword) {
+            return res.status(200).json({ 
+                status: "Error",
+                message: "Passwords do not match"
+            });
+        }
+        console.log("isValidEmail", isValidEmail);
+        const data = await UserService.createUser(req.body);
         return res.status(200).json(data);
     } catch (err) {
         console.log(err);
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,7 +1,32 @@
-const createUser = () => {
-    return new Promise((resolve, reject) => {
+const UserModel = require('../models/UserModel');
+
+const createUser = (newUser) => {
+    return new Promise(async (resolve, reject) => {
+        const { name, email, password, confirmPassword, phone } = newUser;
         try {
-            resolve({});
+            const checkUser = await UserModel.findOne({
+                email: email
+            })
+            if (checkUser !== null) {
+                resolve({
+                    status: "OK",
+                    message: "The Email is already",
+                })
+            }
+            const createdUser = await UserModel.create({
+                name,
+                email,
+                password,
+                confirmPassword,
+                phone
+            });
+            if (createdUser) {
+                resolve({
+                    status: "OK",
+                    message: "User created successfully",
+                    data: createdUser,
+                })
+            }
         } catch (err) {
             console.log(err);
             reject(err);
