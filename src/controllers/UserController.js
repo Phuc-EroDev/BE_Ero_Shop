8aa7d78d176cfe8d65c856abda1d4710a2a6a117
Phuc-EroDev <phucvo140902@gmail.com>
2025-05-17 03:53:33 +0700

    make api delete user with admin

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -74,8 +74,25 @@ const updateUser = async(req, res) => {
     }
 }
 
+const deleteUser = async(req, res) => {
+    try {
+        const userId = req.params.id;
+        if (!userId) {
+            return res.status(200).json({ 
+                status: "Error",
+                message: "The userId is required"
+             });
+        }
+        const response = await UserService.deleteUser(userId);
+        return res.status(200).json(response);
+    } catch (err) {
+        return res.status(404).json({ message: err });
+    }
+}
+
 module.exports = {
     createUser,
     loginUser,
-    updateUser
+    updateUser,
+    deleteUser
 };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -102,8 +102,33 @@ const updateUser = (id, data) => {
     })
 }
 
+const deleteUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await UserModel.findOne({ 
+                _id: id
+             })
+            if (checkUser === null) {
+                resolve({
+                    status: "OK",
+                    message: "The User is not found",
+                })
+            }
+
+            // await UserModel.findByIdAndDelete(id);
+            resolve({
+                status: "OK",
+                message: "Delete user Success",
+            })
+        } catch (err) {
+            reject(err);
+        }
+    })
+}
+
 module.exports = {
     createUser,
     loginUser,
-    updateUser
+    updateUser,
+    deleteUser
 };
