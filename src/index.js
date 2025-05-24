2465c5e5317f45ae9d45332ad5e43349d63e5e35
Phuc-EroDev <phucvo140902@gmail.com>
2025-05-25 03:00:53 +0700

    allow upload file too large (image avatar)

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ app.use(cors({
   origin: 'http://localhost:5173', // FE origin
   credentials: true
 }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
