e3a3322fd902a6a2d9507521cbed37da227d4ef6
Phuc-EroDev <phucvo140902@gmail.com>
2025-06-11 04:48:14 +0700

    add discount in create product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,114 +1,112 @@
 const ProductService = require('../services/ProductService');
 
-const createProduct = async(req, res) => {
-    try {
-        const { name, image, type, price, countInStock, rating, description } = req.body;
-        if(!name || !image || !type || !price || !countInStock || !rating ) {
-            return res.status(400).json({ 
-                status: "Error",
-                message: "Please fill all the fields"
-             });
-        }
-        const response = await ProductService.createProduct(req.body);
-        return res.status(200).json(response);
-    } catch (err) {
-        console.log(err);
-        return res.status(500).json({ message: err });
+const createProduct = async (req, res) => {
+  try {
+    const { name, image, type, price, countInStock, rating, discount, description } = req.body;
+    if (!name || !image || !type || !price || !countInStock || !rating || !discount) {
+      return res.status(400).json({
+        status: 'Error',
+        message: 'Please fill all the fields',
+      });
     }
-}
+    const response = await ProductService.createProduct(req.body);
+    return res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ message: err });
+  }
+};
 
-const updateProduct = async(req, res) => {
-    try {
-        const productId = req.params.id;
-        const data = req.body;
-        if (!productId) {
-            return res.status(400).json({ 
-                status: "Error",
-                message: "The productId is required"
-             });
-        }
-        const response = await ProductService.updateProduct(productId, data);
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
+const updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const data = req.body;
+    if (!productId) {
+      return res.status(400).json({
+        status: 'Error',
+        message: 'The productId is required',
+      });
     }
-}
+    const response = await ProductService.updateProduct(productId, data);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
 
-const getDetailsProduct = async(req, res) => {
-    try {
-        const productId = req.params.id;
-        if (!productId) {
-            return res.status(400).json({ 
-                status: "Error",
-                message: "The productId is required"
-             });
-        }
-        const response = await ProductService.getDetailsProduct(productId);
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
+const getDetailsProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(400).json({
+        status: 'Error',
+        message: 'The productId is required',
+      });
     }
-}
+    const response = await ProductService.getDetailsProduct(productId);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
 
-const getAllTypeProduct = async(req, res) => {
-    try {
-        const response = await ProductService.getAllTypeProduct();
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
+const getAllTypeProduct = async (req, res) => {
+  try {
+    const response = await ProductService.getAllTypeProduct();
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+const getAllProduct = async (req, res) => {
+  try {
+    const { limit, page, sort, filter } = req.query;
+    const response = await ProductService.getAllProduct(limit, page, sort, filter);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const productId = req.params.id;
+    if (!productId) {
+      return res.status(400).json({
+        status: 'Error',
+        message: 'The productId is required',
+      });
     }
-}
+    const response = await ProductService.deleteProduct(productId);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
 
-const getAllProduct = async(req, res) => {
-    try {
-        const { limit , page, sort, filter } = req.query;
-        const response = await ProductService.getAllProduct(limit , page, sort, filter);
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
+const deleteManyProduct = async (req, res) => {
+  try {
+    const data = req.body;
+    if (!data?.ids) {
+      return res.status(400).json({
+        status: 'Error',
+        message: 'The ids is required',
+      });
     }
-}
-
-const deleteProduct = async(req, res) => {
-    try {
-        const productId = req.params.id;
-        if (!productId) {
-            return res.status(400).json({ 
-                status: "Error",
-                message: "The productId is required"
-             });
-        }
-        const response = await ProductService.deleteProduct(productId);
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
-    }
-}
-
- const deleteManyProduct = async(req, res) => {
-    try {
-        const data = req.body;
-        if (!data?.ids) {
-            return res.status(400).json({ 
-                status: "Error",
-                message: "The ids is required"
-             });
-        }
-        const response = await ProductService.deleteManyProduct(data?.ids);
-        return res.status(200).json(response);
-    } catch (err) {
-        return res.status(500).json({ message: err });
-    }
-}
-
-
+    const response = await ProductService.deleteManyProduct(data?.ids);
+    return res.status(200).json(response);
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
 
 module.exports = {
-    createProduct,
-    updateProduct,
-    getDetailsProduct,
-    getAllTypeProduct,
-    getAllProduct,
-    deleteProduct,
-    deleteManyProduct
+  createProduct,
+  updateProduct,
+  getDetailsProduct,
+  getAllTypeProduct,
+  getAllProduct,
+  deleteProduct,
+  deleteManyProduct,
 };
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,210 +1,220 @@
 const ProductModel = require('../models/ProductModel');
 
 const createProduct = (newProduct) => {
-    return new Promise(async (resolve, reject) => {
-        const { name, image, type, price, countInStock, rating, description } = newProduct;
-        try {
-            const checkProduct = await ProductModel.findOne({
-                name: name
-            })
-            if (checkProduct !== null) {
-                resolve({
-                    status: "OK",
-                    message: "The name of product is already",
-                })
-            }
-            const createdProduct = await ProductModel.create({
-                name,
-                image,
-                type,
-                price,
-                countInStock,
-                rating,
-                description
-            });
-            if (createdProduct) {
-                resolve({
-                    status: "OK",
-                    message: "Product created successfully",
-                    data: createdProduct,
-                })
-            }
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    const { name, image, type, price, countInStock, rating, discount, description } = newProduct;
+    try {
+      const checkProduct = await ProductModel.findOne({
+        name: name,
+      });
+      if (checkProduct !== null) {
+        resolve({
+          status: 'OK',
+          message: 'The name of product is already',
+        });
+      }
+      const createdProduct = await ProductModel.create({
+        name,
+        image,
+        type,
+        price,
+        countInStock,
+        rating,
+        discount,
+        description,
+      });
+      if (createdProduct) {
+        resolve({
+          status: 'OK',
+          message: 'Product created successfully',
+          data: createdProduct,
+        });
+      }
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 const updateProduct = (id, data) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkProduct = await ProductModel.findOne({ 
-                _id: id
-             })
-            if (checkProduct === null) {
-                resolve({
-                    status: "OK",
-                    message: "The Product is not found",
-                })
-                return;
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkProduct = await ProductModel.findOne({
+        _id: id,
+      });
+      if (checkProduct === null) {
+        resolve({
+          status: 'OK',
+          message: 'The Product is not found',
+        });
+        return;
+      }
 
-            const updatedProduct = await ProductModel.findByIdAndUpdate(id, data, { new: true });
-            resolve({
-                status: "OK",
-                message: "Success",
-                data: updatedProduct,
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+      const updatedProduct = await ProductModel.findByIdAndUpdate(id, data, { new: true });
+      resolve({
+        status: 'OK',
+        message: 'Success',
+        data: updatedProduct,
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 const getDetailsProduct = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const product = await ProductModel.findOne({ 
-                _id: id
-             })
-            if (product === null) {
-                resolve({
-                    status: "OK",
-                    message: "The Product is not found",
-                })
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const product = await ProductModel.findOne({
+        _id: id,
+      });
+      if (product === null) {
+        resolve({
+          status: 'OK',
+          message: 'The Product is not found',
+        });
+      }
 
-            resolve({
-                status: "OK",
-                message: "Success",
-                data: product,
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+      resolve({
+        status: 'OK',
+        message: 'Success',
+        data: product,
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 const getAllTypeProduct = () => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const allTypeProduct = await ProductModel.distinct("type");
-            resolve({
-                status: "OK",
-                message: "Success",
-                data: allTypeProduct,
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      const allTypeProduct = await ProductModel.distinct('type');
+      resolve({
+        status: 'OK',
+        message: 'Success',
+        data: allTypeProduct,
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
-const getAllProduct = (limit = 8, page = 0, sort, filter) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const totalProduct = await ProductModel.countDocuments();
-            if (sort && filter) {
-                const objectSort = {};
-                objectSort[sort[1]] = sort[0];
-                const objectFilter = {};
-                objectFilter[filter[0]] = { '$regex': filter[1], $options: 'i' };
-                const allProductSortFilter = await ProductModel.find(objectFilter).limit(limit).skip(page * limit).sort(objectSort);
-                return resolve({
-                    status: "OK",
-                    message: "Success",
-                    data: allProductSortFilter,
-                    total: totalProduct,
-                    pageCurrent: parseInt(page) + 1,
-                    totalPage: Math.ceil(totalProduct / limit),
-                })
-            }
-            if (filter) {
-                const objectFilter = {};
-                objectFilter[filter[0]] = { '$regex': filter[1], $options: 'i' };
-                const allProductFilter = await ProductModel.find(objectFilter).limit(limit).skip(page * limit);
-                return resolve({
-                    status: "OK",
-                    message: "Success",
-                    data: allProductFilter,
-                    total: totalProduct,
-                    pageCurrent: parseInt(page) + 1,
-                    totalPage: Math.ceil(totalProduct / limit),
-                })
-            }
-            if (sort) {
-                const objectSort = {};
-                objectSort[sort[1]] = sort[0];
-                const allProductSort = await ProductModel.find().limit(limit).skip(page * limit).sort(objectSort);
-                return resolve({
-                    status: "OK",
-                    message: "Success",
-                    data: allProductSort,
-                    total: totalProduct,
-                    pageCurrent: parseInt(page) + 1,
-                    totalPage: Math.ceil(totalProduct / limit),
-                })
-            }
-            const allProduct = await ProductModel.find().limit(limit).skip(page * limit);
-            resolve({
-                status: "OK",
-                message: "Success",
-                data: allProduct,
-                total: totalProduct,
-                pageCurrent: parseInt(page) + 1,
-                totalPage: Math.ceil(totalProduct / limit),
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+const getAllProduct = (limit = 12, page = 0, sort, filter) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const totalProduct = await ProductModel.countDocuments();
+      if (sort && filter) {
+        const objectSort = {};
+        objectSort[sort[1]] = sort[0];
+        const objectFilter = {};
+        objectFilter[filter[0]] = { $regex: filter[1], $options: 'i' };
+        const allProductSortFilter = await ProductModel.find(objectFilter)
+          .limit(limit)
+          .skip(page * limit)
+          .sort(objectSort);
+        return resolve({
+          status: 'OK',
+          message: 'Success',
+          data: allProductSortFilter,
+          total: totalProduct,
+          pageCurrent: parseInt(page) + 1,
+          totalPage: Math.ceil(totalProduct / limit),
+        });
+      }
+      if (filter) {
+        const objectFilter = {};
+        objectFilter[filter[0]] = { $regex: filter[1], $options: 'i' };
+        const allProductFilter = await ProductModel.find(objectFilter)
+          .limit(limit)
+          .skip(page * limit);
+        return resolve({
+          status: 'OK',
+          message: 'Success',
+          data: allProductFilter,
+          total: totalProduct,
+          pageCurrent: parseInt(page) + 1,
+          totalPage: Math.ceil(totalProduct / limit),
+        });
+      }
+      if (sort) {
+        const objectSort = {};
+        objectSort[sort[1]] = sort[0];
+        const allProductSort = await ProductModel.find()
+          .limit(limit)
+          .skip(page * limit)
+          .sort(objectSort);
+        return resolve({
+          status: 'OK',
+          message: 'Success',
+          data: allProductSort,
+          total: totalProduct,
+          pageCurrent: parseInt(page) + 1,
+          totalPage: Math.ceil(totalProduct / limit),
+        });
+      }
+      const allProduct = await ProductModel.find()
+        .limit(limit)
+        .skip(page * limit);
+      resolve({
+        status: 'OK',
+        message: 'Success',
+        data: allProduct,
+        total: totalProduct,
+        pageCurrent: parseInt(page) + 1,
+        totalPage: Math.ceil(totalProduct / limit),
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 const deleteProduct = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const checkProduct = await ProductModel.findOne({ 
-                _id: id
-             })
-            if (checkProduct === null) {
-                resolve({
-                    status: "OK",
-                    message: "The Product is not found",
-                })
-            }
+  return new Promise(async (resolve, reject) => {
+    try {
+      const checkProduct = await ProductModel.findOne({
+        _id: id,
+      });
+      if (checkProduct === null) {
+        resolve({
+          status: 'OK',
+          message: 'The Product is not found',
+        });
+      }
 
-            await ProductModel.findByIdAndDelete(id);
-            resolve({
-                status: "OK",
-                message: "Delete product Success",
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
+      await ProductModel.findByIdAndDelete(id);
+      resolve({
+        status: 'OK',
+        message: 'Delete product Success',
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 const deleteManyProduct = (ids) => {
-    return new Promise(async (resolve, reject) => {
-        try {
-            await ProductModel.deleteMany({_id: {$in: ids}});
-            resolve({
-                status: "OK",
-                message: "Delete many product Success",
-            })
-        } catch (err) {
-            reject(err);
-        }
-    })
-}
-
+  return new Promise(async (resolve, reject) => {
+    try {
+      await ProductModel.deleteMany({ _id: { $in: ids } });
+      resolve({
+        status: 'OK',
+        message: 'Delete many product Success',
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
 
 module.exports = {
-    createProduct,
-    updateProduct,
-    getDetailsProduct,
-    getAllTypeProduct,
-    getAllProduct,
-    deleteProduct,
-    deleteManyProduct
+  createProduct,
+  updateProduct,
+  getDetailsProduct,
+  getAllTypeProduct,
+  getAllProduct,
+  deleteProduct,
+  deleteManyProduct,
 };
